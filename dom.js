1d3223dbcf1e8839bc6dd031dc067825cb20111a
Shovon15 <shovonrnbd@gmail.com>
2022-08-25 18:16:25 +0600

    add more input field and display

--- dom.js
+++ dom.js
@@ -1,12 +1,13 @@
 function getINputValue(inputId) {
     const getInputId = document.getElementById(inputId);
     const inputValue = getInputId.value;
-    getInputId.value = "";
+    // getInputId.value = "";
     return inputValue;
 }
 
-function generateTable(tableValue1, tableValue2, tableValue3) {
+function generateTable(tableValue1, tableValue2, tableValue3, tableValue4, tableValue5, tableValue6, tableValue7) {
     const row = document.createElement("tr");
+    row.style.border = "1px solid white";
     // const td1 = document.createElement("td");
     // const td2 = document.createElement("td");
     // const td3 = document.createElement("td");
@@ -22,17 +23,25 @@ function generateTable(tableValue1, tableValue2, tableValue3) {
        <td>${tableValue1}</td>
        <td>${tableValue2}</td>
        <td>${tableValue3}</td>
+       <td>${tableValue4}</td>
+       <td>${tableValue5}</td>
+       <td>${tableValue6}</td>
+       <td>${tableValue7}</td>
 
         `;
-
-    document.getElementById("tr").appendChild(row);
+    const tableRow = document.getElementById("tr");
+    tableRow.appendChild(row);
 }
 
 document.getElementById("submit-btn").addEventListener("click", (e) => {
     e.preventDefault();
-    const fNameValue = getINputValue("first_name");
-    const lNameValue = getINputValue("last_name");
-    const ideaValue = getINputValue("idea");
+    const nameValue = getINputValue("name-field");
+    const departmentValue = getINputValue("department-field");
+    const batchValue = getINputValue("batch-field");
+    const phoneValue = getINputValue("phone-field");
+    const projectTitleValue = getINputValue("project-title-field");
+    const technicalValue = getINputValue("technical-field");
+    const ideaTextValue = getINputValue("idea-text-field");
 
-    generateTable(fNameValue, lNameValue, ideaValue);
+    generateTable(nameValue, departmentValue, batchValue, phoneValue, projectTitleValue, technicalValue, ideaTextValue);
 });
